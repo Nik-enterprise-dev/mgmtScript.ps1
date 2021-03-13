bc40c9f237bfbe7be7181e82bebe7c0087eb7ed8
Nik-enterprise-dev <80557956+Nik-enterprise-dev@users.noreply.github.com>
2021-03-13 18:57:40 -0500

    Create SystemInfo.ps1
    Gets System Info from each computer on the domain

---
+++ SystemInfo.ps1
@@ -0,0 +1,7 @@
+Import-Module ActiveDirectory
+$userName = 'nik'
+$userPassword = 'ToastyBoi!'
+$psCreds = ConvertTo-SecureString $userPassword -AsPlainText -Force
+$Computers = New-Object -TypeName "System.Collections.ArrayList"
+$Computer = $(Get-ADComputer -Filter * | Select-Object Name)
+for ($index = -1; $index -lt $Computer.count; $index++) { Invoke-Command -ComputerName $index {systeminfo} }
